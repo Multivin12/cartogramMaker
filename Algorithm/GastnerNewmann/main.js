0e003a47f4d9677d28c9a466b88fdad725e4925a
Multivin15 <vrq16kku@uea.ac.uk>
2019-01-17 15:29:19 +0000

    Pushed all the server files onto the repository

---
+++ Algorithm/GastnerNewmann/main.js
@@ -0,0 +1,82 @@
+const OFFSET = 0.005;
+const DCT2 = require('./DCT2.js');
+const GastnerNewmann = require('./GastnerNewmann.js');
+
+var inputData = null;
+
+/* Function for reading from an external file into inputData*/
+function readpop(filename,xsize,ysize) {
+    inputData = DCT2.initialize2DArray(xsize,ysize);
+
+    //assign the test data
+    for (var i=0;i<xsize;i++) {
+        for (var j=0;j<ysize;j++) {
+            inputData[i][j] = (i+1)*(j+1);
+        }
+    }
+
+    var ix,iy;
+    var n;
+    var mean;
+    var sum=0.0;
+
+    for (iy=0; iy<ysize; iy++) {
+        for (ix=0; ix<xsize; ix++) {
+            sum += inputData[ix][iy];
+        }
+    }
+
+    mean = sum/(xsize*ysize);
+    for (iy=0; iy<ysize; iy++) {
+        for (ix=0; ix<xsize; ix++) {
+            inputData[ix][iy] += OFFSET*mean;
+        }
+    }
+
+}
+
+/*Function to print out the output of the algorithm to stdout*/
+function printOutput(gridx,gridy) {
+    for(var i =0;i<gridx.length;i++) {
+        console.log(gridx[i],gridy[i]);
+    }
+}
+
+/*function to create the grid for the algorithm to use */
+function creategrid(xsize,ysize) {
+    var gridx = DCT2.initializeArray((xsize+1)*(ysize+1));
+    var gridy = DCT2.initializeArray((xsize+1)*(ysize+1));
+    var ix,iy,i;
+
+    for(iy=0,i=0;iy<=ysize;iy++) {
+        for(ix=0;ix<=xsize;ix++) {
+            gridx[i] = ix;
+            gridy[i] = iy;
+            i++
+        }
+    }
+    return [gridx,gridy];
+}
+
+/*Function to launch the gastner-newmann algorithm*/
+function main(densityFile,xsize,ysize) {
+
+    readpop(densityFile,xsize,ysize);
+
+    var obj = new GastnerNewmann();
+
+    //Do the various stages as stated in the documentation of cart.c
+    obj.cart_makews(xsize,ysize);
+    obj.cart_transform(inputData,xsize,ysize);
+    var temp = creategrid(xsize,ysize);
+    var gridx = temp[0];
+    var gridy = temp[1];
+    var tup = obj.cart_makecart(gridx,gridy,(xsize+1)*(ysize+1),xsize,ysize,0);
+
+    gridx = tup[0];
+    gridy = tup[1];
+
+    printOutput(gridx,gridy);
+}
+
+main("GridFile.txt",5,5);
---
+++ server.js
@@ -0,0 +1,46 @@
+//express is for handling requests
+const express = require('express');
+
+//formidable is for handling file uploads
+const formidable = require('formidable');
+
+const app = express();
+const path = require('path');
+
+//homepage
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname+'/HTML_templates/index.html'));
+});
+
+//method for uploading a file
+app.post('/fileupload',(req,res) =>{
+
+    var form = new formidable.IncomingForm();
+    var success = null;
+
+    
+    form.parse(req);
+
+    //The event that is run when a new file has been detected
+    form.on('fileBegin', (name, file) => {
+        if (file.name === "") {
+            success = false;
+        } else {
+            file.path = __dirname + '/uploads/' + file.name;
+            success = true;
+        }
+    });
+
+    //The event that is run when the file upload has been completed
+    form.on('end', () => {
+        if(success) {
+            res.sendFile(__dirname + '/HTML_templates/uploadSuc.html');
+        } else {
+            res.sendFile(__dirname + '/HTML_templates/index.html')
+        }
+    })
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
