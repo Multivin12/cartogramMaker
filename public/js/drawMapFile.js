9b15e7c826b957f4bf39f7f7bcd52c297ff4f821
Multivin15 <vrq16kku@uea.ac.uk>
2019-01-26 11:27:32 +0000

    Started file handling between server and client side javascript

--- public/js/drawMapFile.js
+++ public/js/drawMapFile.js
@@ -1,3 +1,16 @@
-function drawMap(variable) {
-    alert(variable);
+function loadData() {
+    var file = "file://../test.txt";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if(this.readyState === 4) {
+            if(this.status === 200 || this.status == 0) {
+                drawMap(this);
+            }
+        }
+    }
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+function drawMap(data) {
+    alert(data.responseXML);
 }
