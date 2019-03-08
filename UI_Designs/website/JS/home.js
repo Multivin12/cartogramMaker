4ced5dbdd63c89f549d11225d9387a2d68f8f7e9
DESKTOP-CI5CUM0\Vincent Nicholson <vince1998@hotmail.co.uk>
2019-03-08 15:34:22 +0000

    more UI stuff

--- UI_Designs/website/JS/home.js
+++ UI_Designs/website/JS/home.js
@@ -1,9 +1,21 @@
 function init() {
-	var windowHeight = $(window).height();
-	var headerHeight = $("#header").height();
-	var img = $("#galleryImage");
-	alert(windowHeight);
-	alert(headerHeight);
-	img.height = windowHeight - headerHeight;
-	alert(img.height);
+	try {
+		document.getElementById("gallery").removeChild(document.getElementById("gallery").childNodes[document.getElementById("gallery").childNodes.length-1]);
+	} catch (err) {
+	}
+	
+	var img = new Image();
+	var url = "images/FAQImage.png";
+
+	img.onload = function () {
+		var windowHeight = $(window).height();
+		this.height = windowHeight - 89;
+		this.width = $(window).width();
+
+		this.setAttribute("style","position:relative;z-index:-1;bottom:240px;");
+		this.setAttribute("id","galleryImage");
+		document.getElementById("gallery").appendChild(this);
+	};
+
+	img.src = url;
 }
