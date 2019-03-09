fc58c4ebc8e74622864f6e7f8b60f1d3a648b098
Multivin15 <vrq16kku@uea.ac.uk>
2019-03-09 22:10:38 +0000

    photo gallery now working

--- UI_Designs/website/JS/home.js
+++ UI_Designs/website/JS/home.js
@@ -1,21 +1,114 @@
 function init() {
-	try {
-		document.getElementById("gallery").removeChild(document.getElementById("gallery").childNodes[document.getElementById("gallery").childNodes.length-1]);
-	} catch (err) {
-	}
+
+	var windowHeight = $(window).height();
+	var newHeight = windowHeight - $("#header").height()-40;
+
+	$("#gallery img.opaque").attr("style","position:absolute;z-index:-5;left:0%;bottom:0%;width:100%;height:" + newHeight + "px;");
+
+	addHTML(0);
+	//For the selector buttons at the bottom
+	$(document).ready(function() {
+		$("#gallery_controls").on('click', 'button', function() {
+			//Remove all classes from the gallery image
+			$("#gallery img").removeClass("opaque");
+			
+			//get the new image to display
+			var newImage = $(this).index();
 	
-	var img = new Image();
-	var url = "images/FAQImage.png";
+			$("#gallery img").removeAttr("style");
+			//set the class of the image to display to opaque
+			$("#gallery img").eq(newImage).addClass("opaque");
 
-	img.onload = function () {
-		var windowHeight = $(window).height();
-		this.height = windowHeight - 89;
-		this.width = $(window).width();
+			//change the size of the gallery
+			var windowHeight = $(window).height();
+			var newHeight = windowHeight - $("#header").height()-40;
+			$("#gallery img").eq(newImage).attr("style","position:absolute;z-index:-5;left:0%;bottom:0%;width:100%;height:" + newHeight + "px;");
+	
+			$("#gallery_controls button").removeClass("selected");
+			$(this).addClass("selected");
 
-		this.setAttribute("style","position:relative;z-index:-1;bottom:240px;");
-		this.setAttribute("id","galleryImage");
-		document.getElementById("gallery").appendChild(this);
-	};
+			addHTML(newImage);
+		});
+	});
+}
 
-	img.src = url;
+/*This function is called whenever the left button is pressed */
+function translatePictureLeft() {
+	var index = $(".opaque").index();
+
+
+	//Remove all classes from the gallery image
+	$("#gallery img").removeClass("opaque");
+
+	index -= 3;
+	
+
+	if( index === -1) {
+		index = 3;
+	} 
+	
+	$("#gallery img").removeAttr("style");
+	//set the class of the image to display to opaque
+	$("#gallery img").eq(index).addClass("opaque");
+
+	//change the size of the gallery
+	var windowHeight = $(window).height();
+	var newHeight = windowHeight - $("#header").height()-40;
+	$("#gallery img").eq(index).attr("style","position:absolute;z-index:-5;left:0%;bottom:0%;width:100%;height:" + newHeight + "px;");
+
+	$("#gallery_controls button").removeClass("selected");
+	$("#gallery_controls button").eq(index).addClass("selected");
+
+	addHTML(index);
+}
+
+/*This function is called whenever the left button is pressed */
+function translatePictureRight() {
+	var index = $(".opaque").index();
+
+
+	//Remove all classes from the gallery image
+	$("#gallery img").removeClass("opaque");
+
+	index -= 1;
+	
+
+	if( index === 4) {
+		index = 0;
+	} 
+	
+	$("#gallery img").removeAttr("style");
+	//set the class of the image to display to opaque
+	$("#gallery img").eq(index).addClass("opaque");
+
+	//change the size of the gallery
+	var windowHeight = $(window).height();
+	var newHeight = windowHeight - $("#header").height()-40;
+	$("#gallery img").eq(index).attr("style","position:absolute;z-index:-5;left:0%;bottom:0%;width:100%;height:" + newHeight + "px;");
+
+	$("#gallery_controls button").removeClass("selected");
+	$("#gallery_controls button").eq(index).addClass("selected");
+
+	addHTML(index);
+}
+
+/**
+ * Function to add HTML to the image gallery
+ * 
+ * @param  element 
+ */
+function addHTML(imgIndex) {
+	switch(imgIndex) {
+		case 0:
+			$("body").append($("<button/>").attr("style","position:absolute;z-index:5;").append("0"));
+			break;
+		case 1:
+			$("body").append($("<button/>").attr("style","position:absolute;z-index:5;").append("1"));
+			break;
+		case 2:
+			$("body").append($("<button/>").attr("style","position:absolute;z-index:5;").append("2"));
+			break;
+		default:
+			$("body").append($("<button/>").attr("style","position:absolute;z-index:5;").append("3"));
+	}
 }
